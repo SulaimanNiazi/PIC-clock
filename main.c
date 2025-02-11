394cc30283e99fbec4c2e05099472eebe136d448
Muhammad Sulaiman <muhammad.sulaiman@emumba.com>
2025-02-11 22:48:57 +0500

    Initialize PORTB as output and low

--- main.c
+++ main.c
@@ -5,9 +5,23 @@
  * Created on February 11, 2025, 9:03 PM
  */
 
+//**Define pins and ports**
 
+#define displayPort PORTB
+#define displayPortDIR TRISB
+
+//*************************
+
+#define _XTAL_FREQ 20000000
 #include <xc.h>
 
 void main(void) {
-    return;
+    //Initialization of pins and ports
+    
+    displayPortDIR = 0x00;
+    displayPort = 0x00;
+    
+    while(1){
+        
+    }
 }
